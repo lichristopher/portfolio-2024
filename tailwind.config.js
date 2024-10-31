e00f186227cf9096c7b3a14b21abfb3e0a6596e5
Christopher Li <li.christopher.jr@gmail.com>
2024-10-31 14:20:23 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0A0E1A',
+        'light-gray': '#A0AEC0',
+        'purple-accent': '#6C2BD9',
+      },
+    },
+  },
+  plugins: [],
+};
